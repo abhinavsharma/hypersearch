ed7295bfe50a73359380feb555022aeb61381544
Abhinav Sharma <abhinav.sharma@me.com>
2020-03-20 13:57:26 -0700

    message passing and inline pills

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,5 +1,5 @@
 export const LUMOS_API_URL = "https://zy6kcqa01a.execute-api.us-east-2.amazonaws.com/prod/";
-export const LUMOS_APP_URL = "https://app.lumosbrowser.com/messenger/";
+export const LUMOS_APP_URL = "https://localhost:3000/messenger/";
 export const QUERY_PARAM_STRING = "q";
 
 export const MESSAGES = {
@@ -8,7 +8,7 @@ export const MESSAGES = {
 
 export const CONTENT_PAGE_ELEMENT_ID_LUMOS_SIDEBAR = 'lumos_sidebar'
 export const CONTENT_PAGE_ELEMENT_ID_LUMOS_DRAWER = 'lumos_drawer'
-export const CONTENT_PAGE_ELEMENT_ID_GOOGLE_SIDEBAR = 'lumos_sidebar'
+export const CONTENT_PAGE_ELEMENT_ID_LUMOS_HIDDEN = 'lumos_hidden'
 
 export const CONTENT_PAGE_SUBTAB_IFRAME_STYLE_OVERRIDES = `
     // google custom search engines width
@@ -25,7 +25,10 @@ export const STYLE_COLOR_BORDER = '#efefef';
 export const STYLE_COLOR_LINK = 'blue';
 export const STYLE_COLOR_LUMOS_GOLD = '#F9EC69'
 
+export const STYLE_PADDING_PILL = '2px 5px';
 export const STYLE_PADDING_SMALL = '5px';
+
+export const STYLE_BORDER_RADIUS_PILL = '10px'
 
 export const STYLE_WIDTH_SIDEBAR = '300px'
 export const STYLE_HEIGHT_DRAWER = '50px'
