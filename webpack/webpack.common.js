f67688f378b20ad24bb1ed1e0a312b136d55e914
felipewil <felipewil@icloud.com>
2020-03-27 23:27:10 -0300

    Fixes.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -6,11 +6,10 @@ const srcDir = '../src/';
 module.exports = {
     entry: {
         background: path.join(__dirname, srcDir + 'background/background.ts'),
-        content_script: path.join(__dirname, srcDir + 'content/content_script.ts'),
+        content_script: path.join(__dirname, srcDir + 'content/content_script.ts')
     },
     output: {
         path: path.join(__dirname, '../dist/js'),
-        publicPath: '/public/',
         filename: '[name].js'
     },
     module: {
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -6,12 +6,6 @@ const ChromeExtensionReloader  = require('webpack-chrome-extension-reloader');
 module.exports = merge(common, {
     devtool: 'inline-source-map',
     mode: 'development',
-    devServer: {
-        port: 9000,
-        contentBase: 'dist/',
-        writeToDisk: true,
-        hot: true,
-    },
     plugins: [
         new ChromeExtensionReloader({
             port: 9090,
