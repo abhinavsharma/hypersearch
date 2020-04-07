6d263e473ec233f977366dee9e2220d1cd668ce3
Abhinav Sharma <abhinav@laso.ai>
2020-04-07 16:21:28 -0700

    remove optimizations from build config

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,10 +3,4 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
-    optimization: {
-        splitChunks: {
-            name: 'vendor',
-            chunks: "initial"
-        }
-    },
 });
