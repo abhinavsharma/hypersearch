81e273a40d281987f71daf8635bde235cbe7ab8d
Abhinav Sharma <abhinav@laso.ai>
2020-04-07 19:10:10 -0700

    add error catching in helpers.ts

--- src/content/helpers.ts
+++ src/content/helpers.ts
@@ -13,16 +13,46 @@ function swapUrlsForDebuggingInJsonResponse(json: object) : object {
 export async function getAPI(api: string, params = {}): Promise<any> {
     let url: URL = new URL(LUMOS_API_URL + api);
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+    try {
+      const response = await fetch(url.href, {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        // body: JSON.stringify(params) // TODO: may move to this later
+      });
+  
+      const response_json = await response.json();
+  
+      if (response_json) {
+        return swapUrlsForDebuggingInJsonResponse(response_json)
+      }
+    } catch (err) {
+      debug("getAPI error", err)
+      return null
+    }
+    
+  }
+
+
+export async function postAPI(api: string, params = {}, body = {}): Promise<any> {
+
+  try {
+    let url: URL = new URL(LUMOS_API_URL + api);
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
     
     const response = await fetch(url.href, {
-      method: 'GET',
+      method: 'POST',
       mode: 'cors',
       cache: 'no-cache',
       headers: {
         'Content-Type': 'application/json'
       },
       redirect: 'follow',
-      // body: JSON.stringify(params) // TODO: may move to this later
+      body: JSON.stringify(body) // TODO: may move to this later
     });
 
     const response_json = await response.json();
@@ -30,29 +60,11 @@ export async function getAPI(api: string, params = {}): Promise<any> {
     if (response_json) {
       return swapUrlsForDebuggingInJsonResponse(response_json)
     }
+  } catch (err) {
+    debug("postAPI error", err)
+    return null
   }
-
-
-export async function postAPI(api: string, params = {}, body = {}): Promise<any> {
-  let url: URL = new URL(LUMOS_API_URL + api);
-  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
   
-  const response = await fetch(url.href, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow',
-    body: JSON.stringify(body) // TODO: may move to this later
-  });
-
-  const response_json = await response.json();
-
-  if (response_json) {
-    return swapUrlsForDebuggingInJsonResponse(response_json)
-  }
 }
 
 export const runFunctionWhenDocumentReady = (document: Document, callback: Function): void => {
