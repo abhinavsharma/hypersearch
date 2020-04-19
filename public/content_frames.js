e63dd1745362428fe4daa1320fdde61bb510c12b
Abhinav Sharma <abhinav@laso.ai>
2020-04-19 16:12:02 -0700

    hotfix: setting iframe links to target blank

--- public/content_frames.js
+++ public/content_frames.js
@@ -1,6 +1,9 @@
 // makes all iframe links open outside the iframe, used for the sidebar
 if (window.location !== window.parent.location) {
-    document.querySelectorAll('a').forEach((linkElement) => {
-        linkElement.setAttribute("target", "_blank")
-    })
+    window.setInterval(() => {
+        document.querySelectorAll('a').forEach((linkElement) => {
+            linkElement.setAttribute("target", "_blank")
+        })
+    }, 1000)
+    
 }
