1fc8bc1d43f572847f61e59df4f3b552763faf90
navneet-laso <navneet@laso.ai>
2020-06-08 22:07:54 +0530

    👌 IMPROVE: Prettify webpack folder

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,19 +1,18 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const ChromeExtensionReloader  = require('webpack-chrome-extension-reloader');
-
+const ChromeExtensionReloader = require('webpack-chrome-extension-reloader');
 
 module.exports = merge(common, {
-    devtool: 'inline-source-map',
-    mode: 'development',
-    plugins: [
-        new ChromeExtensionReloader({
-            port: 9090,
-            reloadPage: true,
-            entries: {
-                contentScript: 'content_script',
-                background: 'background'
-            }
-        })
-    ],
+  devtool: 'inline-source-map',
+  mode: 'development',
+  plugins: [
+    new ChromeExtensionReloader({
+      port: 9090,
+      reloadPage: true,
+      entries: {
+        contentScript: 'content_script',
+        background: 'background',
+      },
+    }),
+  ],
 });
