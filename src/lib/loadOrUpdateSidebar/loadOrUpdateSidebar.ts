5313c5a1a32d0a7b062db0499804a4f7578a0ed2
Matyas Angyal <matyas@laso.ai>
2021-02-23 17:58:15 +0100

    fix: extract constant, add parenthesis to if statements

--- src/lib/loadOrUpdateSidebar/loadOrUpdateSidebar.ts
+++ src/lib/loadOrUpdateSidebar/loadOrUpdateSidebar.ts
@@ -8,6 +8,8 @@ import { loadPublicFile, runFunctionWhenDocumentReady } from 'utils/helpers';
 import { Sidebar } from 'components/Sidebar/Sidebar';
 import { flipSidebar } from 'lib/flipSidebar/flipSidebar';
 
+const WINDOW_REQUIRED_MIN_WIDTH = 1200;
+
 const style = document.getElementsByTagName('style')[0];
 
 const loadSidebarCss = () => {
@@ -32,7 +34,9 @@ const createSidebar = async (document: Document, tabs: SidebarTab[]) => {
   const sidebarInit = React.createElement(Sidebar, { tabs });
   reactInjector(sidebarRoot, sidebarInit, 'sidebar-root-iframe', style);
 
-  if (window.innerWidth <= 1200) flipSidebar(document, 'hide');
+  if (window.innerWidth <= WINDOW_REQUIRED_MIN_WIDTH) {
+    flipSidebar(document, 'hide');
+  }
 
   nativeBrowserAddReactAppListener({
     window,
@@ -51,11 +55,17 @@ export const loadOrUpdateSidebar = async (document: Document, url: URL) => {
   debug('function call - loadOrUpdateSidebar', document);
 
   const firstChild = document.documentElement.firstChild;
-  if (style === firstChild) document.documentElement.removeChild(style);
+
+  if (style === firstChild) {
+    document.documentElement.removeChild(style);
+  }
 
   const sidebarTabsManager = new SidebarTabsManager();
   sidebarTabsManager.fetchSubtabs(url).then((response) => {
-    if (!response) return;
+    if (!response) {
+      return;
+    }
+
     runFunctionWhenDocumentReady(document, async () => {
       const tabs = await handleSubtabApiResponse(url, document, response);
       !!tabs?.length && createSidebar(document, tabs);
