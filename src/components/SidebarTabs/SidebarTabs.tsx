340e43b3e6238c6ff210b1af016dcd185b52c55b
Matyas Angyal <matyas@laso.ai>
2021-02-26 16:53:38 +0100

    feat(sidebar-toggle): change icon to letter 'I' on minimized sidebar

--- src/components/SidebarTabs/SidebarTabs.tsx
+++ src/components/SidebarTabs/SidebarTabs.tsx
@@ -11,12 +11,15 @@ const { TabPane } = Tabs;
 // !DEV Toggle rendering augmentation tab
 export const SHOW_AUGMENTATION_TAB = false;
 
-export const ExternalAddAugmentationButton: ExternalAddAugmentationButton = ({ className }) => (
+export const ExternalAddAugmentationButton: ExternalAddAugmentationButton = ({
+  className,
+  children,
+}) => (
   <div
     className={`add-augmentation-tab ${className}`}
     onClick={() => window.open('https://airtable.com/shr4dMhlqUYNTduxp')}
   >
-    ➕
+    {children}
   </div>
 );
 
