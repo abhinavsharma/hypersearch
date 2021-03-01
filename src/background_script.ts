7cae206dbfab16905c4f81512c2a8dfe1c40f3b1
Matyas Angyal <matyas@laso.ai>
2021-03-01 19:30:32 +0100

    fix: disable cors on all requests

--- src/background_script.ts
+++ src/background_script.ts
@@ -11,46 +11,75 @@ const USER_AGENT_REWRITE_URL_SUBSTRINGS = Object.values(HOSTNAME_TO_PATTERN).map
 
 export const URL_TO_TAB = {};
 
+// eslint-disable-next-line
+/* @ts-ignore */
+const isFirefox = typeof InstallTrigger !== 'undefined';
+const extraSpec = ['blocking', 'responseHeaders', isFirefox ? null : 'extraHeaders'].filter(
+  (i) => i,
+);
+
 debug('installing listener override response headers');
 // https://gist.github.com/dergachev/e216b25d9a144914eae2#file-manifest-json
 // this is to get around loading pages in iframes that otherwise
 // don't want to be loaded in iframes
 chrome.webRequest.onHeadersReceived.addListener(
   function (details) {
-    const strippedHeaders = ['x-frame-options', 'content-security-policy'];
+    const strippedHeaders = [
+      'x-frame-options',
+      'frame-options',
+      'content-security-policy',
+      'referer-policy',
+    ];
+    const responseHeaders = details.responseHeaders.filter((responseHeader) => {
+      const deleted = !strippedHeaders.includes(responseHeader.name.toLowerCase());
+      return deleted;
+    });
+
     return {
-      responseHeaders: details.responseHeaders.filter(
-        (responseHeader) => !strippedHeaders.includes(responseHeader.name.toLowerCase()),
-      ),
+      responseHeaders: [
+        ...responseHeaders,
+        {
+          name: 'Content-Security-Policy',
+          value: `frame-ancestors *`,
+        },
+        {
+          name: 'Access-Control-Allow-Origin',
+          value: 'no-cors',
+        },
+      ],
     };
   },
   {
     urls: ['<all_urls>'],
+    types: ['sub_frame'],
   },
-  ['blocking', 'responseHeaders'],
+  extraSpec,
 );
 
 debug('installing listener for overriding request headers');
 chrome.webRequest.onBeforeSendHeaders.addListener(
   (details) => {
+    const requestHeaders = details.requestHeaders.map((requestHeader) => {
+      // this is for the search result iframes loaded in the sidebar, we pretend the browser is mobile for them
+      const specialUrl = details.url.includes(SPECIAL_URL_JUNK_STRING);
+      const urlMatchesSearchPattern =
+        specialUrl ||
+        USER_AGENT_REWRITE_URL_SUBSTRINGS.filter((substring) => details.url.includes(substring))
+          .length > 0;
+      if (
+        urlMatchesSearchPattern &&
+        details.frameId > 0 &&
+        requestHeader.name.toLowerCase() === 'user-agent'
+      ) {
+        requestHeader.value =
+          'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36';
+      }
+
+      return requestHeader;
+    });
+
     return {
-      requestHeaders: details.requestHeaders.map((requestHeader) => {
-        // this is for the search result iframes loaded in the sidebar, we pretend the browser is mobile for them
-        const specialUrl = details.url.includes(SPECIAL_URL_JUNK_STRING);
-        const urlMatchesSearchPattern =
-          specialUrl ||
-          USER_AGENT_REWRITE_URL_SUBSTRINGS.filter((substring) => details.url.includes(substring))
-            .length > 0;
-        if (
-          urlMatchesSearchPattern &&
-          details.frameId > 0 &&
-          requestHeader.name.toLowerCase() === 'user-agent'
-        ) {
-          requestHeader.value =
-            'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36';
-        }
-        return requestHeader;
-      }),
+      requestHeaders,
     };
   },
   { urls: ['<all_urls>'] },
@@ -62,12 +91,16 @@ function onUpdatedListener(tabId, changeInfo, tab) {
   debug('function call - onUpdatedListener:', tabId, changeInfo, tab);
   if (changeInfo.url) {
     debug('changeInfo has URL:', changeInfo.url);
-    chrome.tabs.sendMessage(tabId, {
-      data: {
-        command: MESSAGES.BROWSERBG_BROWSERFG_URL_UPDATED,
-        url: changeInfo.url,
+    chrome.tabs.sendMessage(
+      tabId,
+      {
+        data: {
+          command: MESSAGES.BROWSERBG_BROWSERFG_URL_UPDATED,
+          url: changeInfo.url,
+        },
       },
-    });
+      () => debug(chrome.runtime.lastError),
+    );
   }
   URL_TO_TAB[tab.url] = tabId;
 }
