f056e1d60987113d47c84679d8f6c6693b5d15e1
Matyas Angyal <amatyas001@gmail.com>
2021-03-18 18:11:08 +0100

    fix: extract sidebar width to constant

--- src/modules/sidebar/SidebarTabContainer/SidebarTabContainer.tsx
+++ src/modules/sidebar/SidebarTabContainer/SidebarTabContainer.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { EXTENSION_SERP_FILTER_LOADED } from 'utils/constants';
+import { EXTENSION_SERP_FILTER_LOADED, SIDEBAR_WIDTH } from 'utils/constants';
 import SidebarLoader from 'lib/SidebarLoader/SidebarLoader';
 
 export const SidebarTabContainer: SidebarTabContainer = ({ tab }) => {
   return (
     <iframe
       src={unescape(tab.url.href)}
-      width={450}
+      width={SIDEBAR_WIDTH}
       className="insight-tab-iframe"
       onLoad={() => {
         SidebarLoader.sendLogMessage(EXTENSION_SERP_FILTER_LOADED, {
