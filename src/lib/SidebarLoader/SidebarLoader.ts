4d766cf3d3cb5a88067b089e19f32bdedef02b94
Matyas Angyal <amatyas001@gmail.com>
2021-03-30 16:56:52 +0200

    docs: update bad hint

--- src/lib/SidebarLoader/SidebarLoader.ts
+++ src/lib/SidebarLoader/SidebarLoader.ts
@@ -437,7 +437,7 @@ class SidebarLoader {
     });
 
     this.sidebarTabs = newTabs.sort((a, b) => {
-      // ! HINT: 1 === leave order, -1 === put B before A
+      // ! HINT: -1 === leave order, 1 === put B before A
       const bothSuggested = a.isSuggested && b.isSuggested;
       if (a.isSuggested && !b.isSuggested && !a.isAnyUrlAction && b.isAnyUrlAction) return -1;
       if (a.isSuggested && !b.isSuggested && a.isAnyUrlAction && !b.isAnyUrlAction) return 1;
