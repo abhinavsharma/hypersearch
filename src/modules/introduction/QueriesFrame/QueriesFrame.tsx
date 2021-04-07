3acf7840590d7974ce76e7206bb0d1f799cf5d74
Matyas Angyal <amatyas001@gmail.com>
2021-04-07 21:15:03 +0200

    feat(onboarding): add v2 structure

---
+++ src/modules/introduction/QueriesFrame/QueriesFrame.tsx
@@ -0,0 +1,79 @@
+import React, { Suspense, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { Button, List, Typography } from 'antd';
+import { APP_NAME } from 'utils';
+import './QueriesFrame.scss';
+
+const { Title } = Typography;
+
+const SyncOutlined = React.lazy(
+  async () => await import('@ant-design/icons/SyncOutlined').then((mod) => mod),
+);
+
+const LIST_DATA = {
+  'Startups: sources trusted by top founders & investors': [
+    { text: 'how to raise a seed round' },
+    { text: 'how to hire engineers' },
+    { text: 'how to hire marketers' },
+  ],
+
+  'Dev: see sources trusted by engineers, designers, data scientists': [
+    { text: 'best js framework' },
+    { text: 'best machine learning books' },
+  ],
+
+  "Shopping: see real people's perspectives and trusted review sites": [
+    { text: 'best car insurance' },
+    { text: 'best baby monitor' },
+    { text: 'best ev to buy 2021' },
+  ],
+
+  'News: see different perspectives': [{ text: 'will trump run in 2024' }],
+
+  'Misc: learn new things better & faster with insider trusted sources': [
+    { text: 'how to build a bunker' },
+    { text: 'best crypto books' },
+    { text: 'best red wines for beginners' },
+  ],
+};
+
+const entries = Object.entries(LIST_DATA);
+
+export const QueriesFrame = () => {
+  const [index, setIndex] = useState(Math.floor(Math.random() * entries.length));
+
+  const handleRandomize = () => setIndex(Math.floor(Math.random() * entries.length));
+
+  const [key, value] = entries[index];
+
+  const header = (
+    <>
+      <Suspense fallback={null}>
+        <Button type="link" icon={<SyncOutlined />} onClick={handleRandomize}></Button>
+      </Suspense>
+      <Title level={3}>{key}</Title>
+    </>
+  );
+
+  return (
+    <>
+      <Helmet>
+        <title>{APP_NAME} - Ready!</title>
+      </Helmet>
+      <div id="queries-frame-container">
+        <Title level={2}>Ready! Try some queries</Title>
+        <List
+          header={header}
+          dataSource={value}
+          renderItem={(item) => (
+            <List.Item>
+              <a target="_blank" href={'https://www.google.com/search?q=' + item.text}>
+                {item.text}
+              </a>
+            </List.Item>
+          )}
+        />
+      </div>
+    </>
+  );
+};
---
+++ src/modules/introduction/WelcomeFrame/WelcomeFrame.tsx
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from 'react';
+import { Typography, Button } from 'antd';
+import Typist from 'react-typist';
+import { StepContext } from 'modules/introduction';
+import 'antd/dist/antd.dark.min.css';
+import './WelcomeFrame.scss';
+
+const { Title } = Typography;
+
+export const WelcomeFrame = () => {
+  const [titleLoaded, setTitleLoaded] = useState<boolean>(false);
+  const stepContext = useContext(StepContext);
+
+  const handleTyped = () => setTitleLoaded(true);
+
+  const handleNext = () => stepContext.setCurrentStep(1);
+
+  return (
+    <div id="welcome-frame-container">
+      <Title level={1} className="main-title">
+        <Typist cursor={{ show: false }} avgTypingDelay={200} onTypingDone={handleTyped}>
+          Hello!
+        </Typist>
+      </Title>
+      <div id="fade-section" className={titleLoaded ? 'fade-section-visible' : ''}>
+        <Title level={2}>Prepare to supercharge search</Title>
+        <Button
+          type="ghost"
+          shape="round"
+          size="large"
+          className="step-button"
+          onClick={handleNext}
+        >
+          Get Started
+        </Button>
+      </div>
+    </div>
+  );
+};
