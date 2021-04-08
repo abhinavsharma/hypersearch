885e46c07fb11ee33ea185b72203f56585d4ae2e
Abhinav Sharma <abhinav@laso.ai>
2021-04-08 08:28:59 -0700

    hotfix style tweaks to sidebar toggle

--- src/modules/sidebar/SidebarToggleButton/SidebarToggleButton.tsx
+++ src/modules/sidebar/SidebarToggleButton/SidebarToggleButton.tsx
@@ -4,7 +4,7 @@
  * @license (C) Insight
  * @version 1.0.0
  */
-import React from 'react';
+import React, { Suspense } from 'react';
 import List from 'antd/lib/list';
 import Button from 'antd/lib/button';
 import Divider from 'antd/lib/divider';
@@ -22,6 +22,10 @@ import 'antd/lib/divider/style/index.css';
 import 'antd/lib/button/style/index.css';
 import './SidebarToggleButton.scss';
 import {} from 'utils';
+
+const DoubleLeftOutlined = React.lazy(
+  async () => await import('@ant-design/icons/DoubleLeftOutlined').then((mod) => mod),
+);
 
 export const SidebarToggleButton: SidebarToggleButton = ({ tabs }) => {
   const handleClick = () => {
@@ -41,7 +45,14 @@ export const SidebarToggleButton: SidebarToggleButton = ({ tabs }) => {
 
   return getFirstValidTabIndex(tabs) !== '0' ? (
     <div onClick={handleClick} className="insight-sidebar-toggle-button">
-      <div className="insight-sidebar-toggle-appname">{APP_NAME}</div>
+      <div className="insight-sidebar-toggle-appname">
+        <Suspense fallback={null}>
+          <DoubleLeftOutlined />
+        </Suspense>
+        <span className="insight-sidebar-toggle-appname-text">
+          {APP_NAME}
+        </span>
+      </div>
       <List
         itemLayout="horizontal"
         dataSource={tabs.filter(({ url }) => url?.href !== HIDE_TAB_FAKE_URL)}
