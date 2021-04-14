22bfa09a5ab8961a0ae4538c000d1c867e5fe989
Abhinav Sharma <abhinav@laso.ai>
2021-04-14 11:17:03 -0700

    hotfix: more clear privacy explainer

--- src/modules/introduction/ToggleAnonymousQueries/ToggleAnonymousQueries.tsx
+++ src/modules/introduction/ToggleAnonymousQueries/ToggleAnonymousQueries.tsx
@@ -1,7 +1,15 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { Suspense, useCallback, useEffect, useState } from 'react';
 import { Switch, Typography } from 'antd';
 import { SYNC_PRIVACY_KEY } from 'utils';
 import './ToggleAnonymousQueries.scss';
+
+const CheckCircleFilled = React.lazy(
+  async () => await import('@ant-design/icons/CheckCircleFilled').then((mod) => mod),
+);
+
+const WarningOutlined = React.lazy(
+  async () => await import('@ant-design/icons/WarningOutlined').then((mod) => mod),
+);
 
 const { Title } = Typography;
 
@@ -27,17 +35,33 @@ export const ToggleAnonymousQueries = () => {
   return (
     <div id="privacy-toggle-container">
       <Switch className="privacy-toggle-button" checked={checked} onChange={handleToggle} />
-      <Title level={3}>{checked ? 'Anonymized Server Calls' : 'Local Processing'}</Title>
+      <Title level={3}>{checked ? 
+      <>
+        <Suspense fallback={null}>
+          <CheckCircleFilled style={{'color': "#52c41a"}} />
+        </Suspense>
+        &nbsp;Use server suggestions
+      </> : <>
+        <Suspense fallback={null}>
+          <WarningOutlined />
+        </Suspense>
+        &nbsp;Do not use sever suggestions
+      </>}</Title>
       {checked ? (
-        <>
-          <p>More and better suggestions.</p>
-          <p>We don't log any identifiable data.</p>
-        </>
+        <div className="privacy-explainer">
+          <p>Recommended for most people, even very privacy sensitive users.</p>
+          <p>Search is computationally complex. 
+            While we allow for 100% client side processing, in order to get the best results, we (like any major search engine, even DuckDuckGo) need to process queries on a server and see which results were clicked on.
+          </p>
+          <p>
+          Our logging standards are stricter than any search engine. We only log dictionary word queries, have no trackers, don't log IP addresses, and delete all logs after 1 month.
+          </p>
+        </div>
       ) : (
-        <>
-          <p>Fewer suggestions but maximum possible privacy.</p>
-          <p>No data is ever sent to our servers.</p>
-        </>
+        <div className="privacy-explainer">
+          <p>Only recommended for local use.</p>
+          <p>The URLs you visit are never shared with our servers. This limits the suggestions we can make.</p>
+        </div>
       )}
     </div>
   );
