c62434ec47ec73c6b98bca6e39eaf0a9977f98f7
Abhinav Sharma <abhinav@laso.ai>
2021-04-20 15:59:29 -0700

    add icon for suggested extensions (#270)
    * add icon for suggested extensions
    
    * fix tooltip

--- src/modules/sidebar/SidebarTabTitle/SidebarTabTitle.tsx
+++ src/modules/sidebar/SidebarTabTitle/SidebarTabTitle.tsx
@@ -1,11 +1,19 @@
 import Tooltip from 'antd/lib/tooltip';
-import React from 'react';
+import React, { Suspense } from 'react';
 import './SidebarTabTitle.scss';
 import 'antd/lib/tooltip/style/index.css';
+import { APP_NAME } from 'utils/constants';
 
-const insertNewlineAfterEmoji = (s) => {
-  return s?.replace(/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])(.*)/g, '$1\n$2');
+const insertNewlineAfterEmoji = (s, showVerified: boolean) => {
+  return <div>
+  {showVerified && <Suspense fallback={null}><CloudDownloadOutlined /></Suspense>}
+  {s?.replace(/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])(.*)/g, '$1\n$2')}
+</div>
 };
+
+const CloudDownloadOutlined = React.lazy(
+  async () => await import('@ant-design/icons/CloudDownloadOutlined').then((mod) => mod),
+);
 
 export const SidebarTabTitle: SidebarTabTitle = ({ tab, index, activeKey, setActiveKey }) => {
   const handleClick = () => setActiveKey((index + 1).toString());
@@ -20,12 +28,12 @@ export const SidebarTabTitle: SidebarTabTitle = ({ tab, index, activeKey, setAct
         }`}
       >
         {!tab.augmentation?.installed ? (
-          <Tooltip title={'Suggested'} destroyTooltipOnHide={{ keepParent: false }}>
-            {insertNewlineAfterEmoji(tab.title)}
+          <Tooltip title={`Lens suggested by ${APP_NAME}`} destroyTooltipOnHide={{ keepParent: false }}>
+            {insertNewlineAfterEmoji(tab.title, true)}
           </Tooltip>
         ) : (
-          <Tooltip title={'Local'} destroyTooltipOnHide={{ keepParent: false }}>
-            {insertNewlineAfterEmoji(tab.title)} {`\u00a0◾`}
+          <Tooltip title={'Local Lens'} destroyTooltipOnHide={{ keepParent: false }}>
+            {insertNewlineAfterEmoji(tab.title, false)}
           </Tooltip>
         )}
       </span>
