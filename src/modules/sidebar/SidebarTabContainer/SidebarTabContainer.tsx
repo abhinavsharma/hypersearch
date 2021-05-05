74c6cd42e71ec4cca2e6850fee85a9c0fa1c7f31
Mátyás Angyal <amatyas001@gmail.com>
2021-05-05 21:46:01 +0200

    fix(redirect-inicator): prevent iframe to redirect top level location and hide inicator (#331)

--- src/modules/sidebar/SidebarTabContainer/SidebarTabContainer.tsx
+++ src/modules/sidebar/SidebarTabContainer/SidebarTabContainer.tsx
@@ -26,6 +26,7 @@ export const SidebarTabContainer: SidebarTabContainer = ({ tab }) => {
   return tab.url.href !== HIDE_TAB_FAKE_URL ? (
     <iframe
       ref={frameRef}
+      sandbox="allow-forms allow-presentation allow-scripts allow-storage-access-by-user-activation allow-same-origin"
       src={decodeSpace(tab.url.href)}
       className="insight-tab-iframe"
       onLoad={() => {
