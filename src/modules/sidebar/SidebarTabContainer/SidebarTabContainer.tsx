5376b3dfe118246206b89dc26bd9798619aba2d4
Mátyás Angyal <amatyas001@gmail.com>
2021-05-06 22:49:32 +0200

    fix(iframes): add allow-popup rule to sandbox prop (#336)

--- src/modules/sidebar/SidebarTabContainer/SidebarTabContainer.tsx
+++ src/modules/sidebar/SidebarTabContainer/SidebarTabContainer.tsx
@@ -26,7 +26,7 @@ export const SidebarTabContainer: SidebarTabContainer = ({ tab }) => {
   return tab.url.href !== HIDE_TAB_FAKE_URL ? (
     <iframe
       ref={frameRef}
-      sandbox="allow-forms allow-presentation allow-scripts allow-same-origin"
+      sandbox="allow-forms allow-presentation allow-scripts allow-same-origin allow-popups"
       src={decodeSpace(tab.url.href)}
       className="insight-tab-iframe"
       onLoad={() => {
