1cdb202134420bc9840ec5993b8b23d1e36d9ee5
Matyas Angyal <amatyas001@gmail.com>
2021-06-04 19:05:04 +0200

    fix(hover-auto-scroll): consider only trusted sites and ignore searched results

--- src/scripts/results.ts
+++ src/scripts/results.ts
@@ -24,6 +24,7 @@ import {
   INSIGHT_SEARCHED_DOMAIN_SELECTOR,
   INSIGHT_SEARCHED_RESULT_SELECTOR,
   INSIGHT_SEARCH_BY_SELECTOR,
+  MY_TRUSTLIST_ID,
   PROCESS_SERP_OVERLAY_MESSAGE,
   REMOVE_HIDE_DOMAIN_OVERLAY_MESSAGE,
   REMOVE_SEARCHED_DOMAIN_MESSAGE,
@@ -224,9 +225,8 @@ import { processSerpResults } from 'utils/processSerpResults/processSerpResults'
             openedAlready = true;
             const firstSearchedResult = (Array.from(
               document.querySelectorAll(`[${INSIGHT_SEARCH_BY_SELECTOR}]`),
-            ) as HTMLDivElement[]).find(
-              (result: HTMLElement) =>
-                result?.getAttribute(INSIGHT_SEARCH_BY_SELECTOR)?.split(' ').length,
+            ) as HTMLDivElement[]).find((result: HTMLElement) =>
+              result?.getAttribute(INSIGHT_SEARCH_BY_SELECTOR)?.includes(MY_TRUSTLIST_ID),
             );
             firstSearchedResult?.getAttribute(INSIGHT_RESULT_URL_SELECTOR);
             if (firstSearchedResult) {
