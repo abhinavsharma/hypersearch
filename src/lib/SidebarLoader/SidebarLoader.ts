2d13168758f95326221cf048d12886953a21b1f3
Matyas Angyal <amatyas001@gmail.com>
2021-06-11 17:45:53 +0200

    feat(onboarding): remove license step, init login when email set

--- src/lib/SidebarLoader/SidebarLoader.ts
+++ src/lib/SidebarLoader/SidebarLoader.ts
@@ -279,11 +279,10 @@ class SidebarLoader {
   }
 
   public get maxAvailableSpace() {
-    const resultWidth = (
-      this.document
-        ?.querySelector(this.customSearchEngine.querySelector?.desktop)
-        ?.closest(this.customSearchEngine.querySelector?.result_container_selector) as HTMLElement
-    )?.offsetWidth;
+    const resultWidth = (this.document
+      ?.querySelector(this.customSearchEngine.querySelector?.desktop)
+      ?.closest(this.customSearchEngine.querySelector?.result_container_selector) as HTMLElement)
+      ?.offsetWidth;
 
     const maxWidth = window.innerWidth - 300;
 
@@ -430,7 +429,7 @@ class SidebarLoader {
         case ACTION_KEYS.SEARCH_ALSO:
           {
             const url = AugmentationManager.processSearchAlsoActionString(
-              action.value[0] as unknown as CustomSearchEngine['search_engine_json'],
+              (action.value[0] as unknown) as CustomSearchEngine['search_engine_json'],
             );
             url.searchParams.append(SPECIAL_URL_JUNK_STRING, SPECIAL_URL_JUNK_STRING);
             urls.unshift(url);
@@ -467,9 +466,9 @@ class SidebarLoader {
       switch (action.key) {
         case ACTION_KEYS.SEARCH_DOMAINS:
           customSearchUrl.searchParams.append(SPECIAL_URL_JUNK_STRING, SPECIAL_URL_JUNK_STRING);
-          this.publicationSlices[augmentation.id][customSearchUrl.href] = action.value.map(
-            (value) => removeProtocol(value),
-          );
+          this.publicationSlices[augmentation.id][
+            customSearchUrl.href
+          ] = action.value.map((value) => removeProtocol(value));
           urls.push(customSearchUrl);
           break;
         case ACTION_KEYS.SEARCH_APPEND:
